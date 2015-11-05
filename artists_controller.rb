1f9111b6cbf68b31fa145a668fa6d6ebea71e82f
Starr Z. Chen <chenstarr@gmail.com>
2015-11-05 14:41:26 -0500

    New artist page working (I think)

--- artists_controller.rb
+++ artists_controller.rb
@@ -1,11 +1,29 @@
-# require "pry"
-
+#artist index
 get '/artists' do
   @artists = Artist.all
   erb :"artists/index"
 end
 
+#new artist page
+get '/artists/new' do
+  erb :"artists/new"
+end
+
+# new artist form
+post '/artists/new' do
+  @name = params[:name]
+  @photo_url = params[:photo_url]
+  @nationality = params[:nationality]
+  @new_artist = Artist.create(name: @name, photo_url: @photo_url, nationality: @nationality)
+  redirect "/artists/" + @new_artist.id.to_s
+end
+
 get '/artists/:id' do
   @artist = Artist.find(params[:id])
   erb :"artists/artist_id"
+end
+
+get '/artists/edit' do
+  @artist = Artist.find(params[:id])
+  erb :"artists/edit"
 end
