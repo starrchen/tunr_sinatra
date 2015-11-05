b1e5a197f8f38c13ae75e0c1192519ae294be165
Starr Z. Chen <chenstarr@gmail.com>
2015-11-05 15:09:55 -0500

    Edit artist working (I think)

--- artists_controller.rb
+++ artists_controller.rb
@@ -1,10 +1,11 @@
-#artist index
+# artist index
 get '/artists' do
   @artists = Artist.all
   erb :"artists/index"
 end
 
-#new artist page
+
+# new artist page
 get '/artists/new' do
   erb :"artists/new"
 end
@@ -18,12 +19,24 @@ post '/artists/new' do
   redirect "/artists/" + @new_artist.id.to_s
 end
 
+# edit artist page
+get '/artists/:id/edit' do
+  @artist = Artist.find(params[:id])
+  erb :"artists/edit"
+end
+
+# edit artist form
+put '/artists/:id' do
+  @name = params[:name]
+  @photo_url = params[:photo_url]
+  @nationality = params[:nationality]
+  @artist = Artist.find(params[:id])
+  @artist.update(name: @name, photo_url: @photo_url, nationality: @nationality)
+  redirect "/artists/" + @artist.id.to_s
+end
+
+# show artist info
 get '/artists/:id' do
   @artist = Artist.find(params[:id])
   erb :"artists/artist_id"
-end
-
-get '/artists/edit' do
-  @artist = Artist.find(params[:id])
-  erb :"artists/edit"
 end
